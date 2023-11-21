4f2749a06f6f88f671218a7d655b41d25558d8c2
yashadityasinghnegi <ynegi100@uottawa.ca>
2023-11-20 19:48:12 -0500

    Sign up page colour changes

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -81,7 +81,7 @@ return (
                         />
                     </div>
                     <button
-                        className="w-full bg-yellow-500 text-black px-4 py-2 rounded-md font-bold hover:bg-blue-600 transition-colors"
+                        className="w-full bg-[#FEBD2F] text-black px-4 py-2 rounded-md font-bold hover:bg-blue-600 transition-colors"
                         type="submit"
                         disabled={busy}
                         style={{ opacity: busy ? 0.5 : 1 }}
